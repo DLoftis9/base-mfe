8ca3136905b48f5ec3778630cd18940e0eefe97e
Darius Loftis <darius.loftis@sharp.com>
2021-09-21 13:37:11 -0700

    updated publication port, updated configs publicPaths for routing

--- home/config/webpack.dev.js
+++ home/config/webpack.dev.js
@@ -1,30 +1,33 @@
-const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 3001,
-    historyApiFallback: {
-      index: "index.html",
-    },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "home",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./Home": "./src/bootstrap",
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+	mode: 'development',
+	output: {
+		publicPath: 'http://localhost:3001',
+	},
+	devServer: {
+		port: 3001,
+		historyApiFallback: {
+			index: 'index.html',
+		},
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'home',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./Home': './src/bootstrap',
+			},
+			shared: packageJson.dependencies,
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
 };
 
 module.exports = merge(commonConfig, devConfig);
--- publication/config/webpack.dev.js
+++ publication/config/webpack.dev.js
@@ -3,18 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8081,
-    historyApiFallback: {
-      index: 'index.html',
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+	mode: 'development',
+	output: {
+		publicPath: 'http://localhost:3002',
+	},
+	devServer: {
+		port: 3002,
+		historyApiFallback: {
+			index: 'index.html',
+		},
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
 };
 
 module.exports = merge(commonConfig, devConfig);
